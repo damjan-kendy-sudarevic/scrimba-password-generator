022a66a3951027f2d8fe28df267a25e0f45e8157
Damjan Sudarević <sudarevic.damjan@gmail.com>
2025-08-30 13:45:05 +0200

    Toggle symbols and numbers functionality

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 const generateButton = document.getElementById("generate-btn");
-const resultContainer = document.getElementById("result-container");
+const numbersCheckbox = document.getElementById("include-numbers");
+const symbolsCheckbox = document.getElementById("include-symbols");
+const resultContainer = document.getElementById("results");
 const resultElements = resultContainer.getElementsByTagName("p");
-
-var uppercase = [...Array(26)].map((_, x) => { return String.fromCharCode(x + 65); });
-var lowercase = [...Array(26)].map((_, x) => { return String.fromCharCode(x + 97); });
-const allLetters = uppercase.concat(lowercase);
 
 const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 const numbers = [...Array(10)].map((_, x) => { return String(x); });
+const letters = [].concat(
+    [...Array(26)].map((_, x) => { return String.fromCharCode(x + 65); }),
+    [...Array(26)].map((_, x) => { return String.fromCharCode(x + 97); })
+);
+
+function getCharacters() {
+    let characters = letters;
+    if (numbersCheckbox.checked)
+        characters = characters.concat(numbers);
+    if (symbolsCheckbox.checked)
+        characters = characters.concat(symbols);
+    return characters;
+}
 
 function generatePassword(length) {
-    var password = "";
-    var allCharacters = [].concat(allLetters, symbols, numbers);
-    for (var i = 0; i < length; i++) {
-        var characterIndex = Math.floor(Math.random() * allCharacters.length);
-        password += allCharacters[characterIndex];
+    let password = "";
+    let characters = getCharacters();
+    for (let i = 0; i < length; i++) {
+        let characterIndex = Math.floor(Math.random() * characters.length);
+        password += characters[characterIndex];
     }
     return password;
 }
 
 generateButton.addEventListener("click", () => {
-    for (var i = 0; i < resultElements.length; i ++) {
+    for (let i = 0; i < resultElements.length; i ++) {
         resultElements[i].textContent = generatePassword(15);
     }
 });
